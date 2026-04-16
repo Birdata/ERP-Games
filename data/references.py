7dd78ae5e5dfed5c4de14211d650ef2496ead8f3
Jakob Breum Møller <jakob.breum.moller@gmail.com>
2026-04-16 10:53:26 +0200

    Update reference sequences for B8555G23 and A1234H15ny

--- data/references.py
+++ data/references.py
@@ -1,7 +1,7 @@
 REFERENCE_SEQUENCES = {
-    "B8555G23": ["blue", "blue", "yellow", "yellow", "green", "white"],
+    "B8555G23": ["blue", "blue", "yellow", "white", "white", "yellow"],
     "B2378F81": ["white", "purple", "red", "green", "blue"],
     "B1375A23": ["orange", "orange", "purple", "yellow", "purple"],
-    "A1234H15": ["red", "white", "red", "yellow"],
+    "A1234H15": ["red", "red", "white", "yellow"],
     "B1375A22": ["red", "green", "green", "white"],
 }
